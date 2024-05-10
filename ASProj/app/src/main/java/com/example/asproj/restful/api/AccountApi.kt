ad19def6b6ae8432fbcdfe583183b836c7a841c0
czw <1848403165@qq.com>
2024-05-10 15:36:36 +0800

    项目初始化2

--- ASProj/app/src/main/java/com/example/asproj/restful/api/AccountApi.kt
+++ ASProj/app/src/main/java/com/example/asproj/restful/api/AccountApi.kt
@@ -1,6 +1,8 @@
 package com.example.asproj.restful.api
 
+import com.example.asproj.model.UserProfile
 import com.example.asproj.restful.annotation.Filed
+import com.example.asproj.restful.annotation.GET
 import com.example.asproj.restful.annotation.HiCall
 import com.example.asproj.restful.annotation.POST
 
@@ -8,10 +10,16 @@ interface AccountApi {
 
     @POST("user/login")
     fun login(
-        @Filed("username") userName: String,
+        @Filed("userName") userName: String,
         @Filed("password") password: String
     ):HiCall<String>
 
     @POST("user/registration")
-    fun register(@Filed("username") userName: String, @Filed("password") password: String):HiCall<String>
+    fun register(@Filed("userName") userName: String, @Filed("password") password: String,@Filed("phoneNum") phoneNum:String):HiCall<String>
+
+   @POST("user/logout")
+   fun logout():HiCall<String>
+
+   @GET("user/profile")
+   fun getProfile() :HiCall<UserProfile>
 }
